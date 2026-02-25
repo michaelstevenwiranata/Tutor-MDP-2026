3ac6418bf9ae60a8228e750ec1dae259283a29b6
orangkeren111 <168069107+orangkeren111@users.noreply.github.com>
2026-02-25 21:00:00 +0700

    companion object

--- Minggu1/app/src/main/java/com/example/minggu1/SpacePart.kt
+++ Minggu1/app/src/main/java/com/example/minggu1/SpacePart.kt
@@ -4,4 +4,12 @@ abstract class SpacePart (val nama: String, val deskripsi: String){
     fun getFungsiPart(): String{
         return "Fungsi mesin ini adalah "+ deskripsi;
     }
+
+    companion object Factory {
+        fun createSayap(name: String): SayapPart {
+            // Can access private constructors/members of User
+            return SayapPart(name, "Menjaga kestabilan saat memasuki atmosfer planet.")
+        }
+        const val DEFAULT_NAME = "Nama" // Example of a constant
+    }
 }
